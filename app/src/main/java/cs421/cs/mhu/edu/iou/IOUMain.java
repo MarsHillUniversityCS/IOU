daf3f24541da3bbfc05d0a4c24436c67944eaed6
Marty Gilbert <martygilbert@gmail.com>
2017-11-28 08:38:18 -0500

    Initial commit

---
+++ app/src/main/java/cs421/cs/mhu/edu/iou/IOUMain.java
@@ -0,0 +1,13 @@
+package cs421.cs.mhu.edu.iou;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IOUMain extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ioumain);
+    }
+}
