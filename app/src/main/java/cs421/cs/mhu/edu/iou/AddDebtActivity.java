8ca72ec0c0b5bddc05c30dc6d511bfbaa8dca40d
Brandon Watkins <brandonwatkins1@mhu.edu>
2017-12-05 13:13:42 -0500

    Code as of 5th Dec

---
+++ app/src/main/java/cs421/cs/mhu/edu/iou/AddDebtActivity.java
@@ -0,0 +1,366 @@
+package cs421.cs.mhu.edu.iou;
+
+import android.Manifest;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.provider.ContactsContract;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+public class AddDebtActivity extends Activity {
+
+    private static final String TAG = AddDebtActivity.class.getSimpleName();
+    private static final int REQUEST_CODE_PICK_CONTACTS = 1;
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 2;
+    private Uri uriContact;
+    private String contactID;
+
+    private Button selectContactBtn;
+    private Button selectDateBtn;
+    private Button selectTimeBtn;
+
+    private TextView amountLbl;
+    private TextView titleLbl;
+    private TextView dateLbl;
+    private TextView timeLbl;
+    private TextView dateSelectedLbl;
+    private TextView timeSelectedLbl;
+
+    private EditText selectedContactName;
+    private EditText selectedContactNumber;
+
+    private EditText amount;
+    private EditText title;
+    private EditText time;
+
+    private DatePickerDialog datePickerDialog;
+    int mDay;
+    int mMonth;
+    int mYear;
+    int mHour;
+    int mMinute;
+    int mAmPm;
+
+    String AMPM;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_debt);
+
+        requestPermissions();
+
+        final Calendar c = Calendar.getInstance();
+
+        mDay    = c.get(Calendar.DAY_OF_MONTH);
+        mMonth  = c.get(Calendar.MONTH);
+        mYear   = c.get(Calendar.YEAR);
+        mHour   = c.get(Calendar.HOUR);
+        mMinute = c.get(Calendar.MINUTE);
+        mAmPm   = c.get(Calendar.AM_PM);
+
+        amountLbl = (TextView) findViewById(R.id.amountLbl);
+        titleLbl = (TextView) findViewById(R.id.titleLbl);
+        dateLbl = (TextView) findViewById(R.id.dateLbl);
+        timeLbl = (TextView) findViewById(R.id.timeLbl);
+        dateSelectedLbl = (TextView) findViewById(R.id.dateSelectedLbl);
+        timeSelectedLbl = (TextView) findViewById(R.id.timeSelectedLbl);
+
+        selectedContactName = (EditText) findViewById(R.id.selectedContactName);
+        selectedContactNumber = (EditText) findViewById(R.id.selectedContactNumber);
+
+        amount = (EditText) findViewById(R.id.amountEditText);
+        title = (EditText) findViewById(R.id.titleEditText);
+
+        selectContactBtn = (Button) findViewById(R.id.selectContactBtn);
+        selectDateBtn = (Button) findViewById(R.id.selectDateBtn);
+        selectTimeBtn = (Button) findViewById(R.id.selectTimeBtn);
+
+        dateSelectedLbl.setText(mDay + "/" + (mMonth + 1) + "/" + mYear);
+
+        //Determines AM or PM for display
+        if(mAmPm == 0) {
+            AMPM = "AM";
+        } else if (mAmPm == 1) {
+            AMPM = "PM";
+        }
+
+        timeSelectedLbl.setText(mHour + ":" + mMinute + " " + AMPM);
+
+
+    }
+
+    public void onClickSelectContact(View btnSelectContact) {
+
+        // using native contacts selection
+        // Intent.ACTION_PICK = Pick an item from the data, returning what was selected.
+        startActivityForResult(new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI), REQUEST_CODE_PICK_CONTACTS);
+
+        Log.d("AddNewContact", "Contacts: " + ContactsContract.Contacts.CONTENT_URI);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_PICK_CONTACTS && resultCode == RESULT_OK) {
+            Log.d(TAG, "Response: " + data.toString());
+            uriContact = data.getData();
+
+            Log.d("AddNewContact", "URI: " + uriContact.toString());
+
+            retrieveContactName();
+            retrieveContactNumber();
+            //retrieveContactPhoto();
+
+        }
+    }
+
+    private void retrieveContactNumber() {
+
+        String contactNumber = null;
+
+        // getting contacts ID
+        Cursor cursorID = getContentResolver().query(uriContact,
+                new String[]{ContactsContract.Contacts._ID},
+                null, null, null);
+
+        if (cursorID.moveToFirst()) {
+
+            contactID = cursorID.getString(cursorID.getColumnIndex(ContactsContract.Contacts._ID));
+        }
+
+        cursorID.close();
+
+        Log.d(TAG, "Contact ID: " + contactID);
+
+        // Using the contact ID now we will get contact phone number
+        Cursor cursorPhone = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER},
+
+                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ? AND " +
+                        ContactsContract.CommonDataKinds.Phone.TYPE + " = " +
+                        ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE,
+
+                new String[]{contactID},
+                null);
+
+        if (cursorPhone.moveToFirst()) {
+            contactNumber = cursorPhone.getString(cursorPhone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+        }
+
+        cursorPhone.close();
+
+        Log.d(TAG, "Contact Phone Number: " + contactNumber);
+
+        selectedContactNumber.setText(contactNumber);
+    }
+
+    private void retrieveContactName() {
+
+        String contactName = null;
+
+        // querying contact data store
+        Cursor cursor = getContentResolver().query(uriContact, null, null, null, null);
+
+        if (cursor.moveToFirst()) {
+
+            // DISPLAY_NAME = The display name for the contact.
+            // HAS_PHONE_NUMBER =   An indicator of whether this contact has at least one phone number.
+
+            contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+        }
+
+        cursor.close();
+
+        Log.d(TAG, "Contact Name: " + contactName);
+
+        selectedContactName.setText(contactName);
+    }
+
+    public void requestPermissions() {
+
+        // Here, thisActivity is the current activity
+        if (this.checkSelfPermission(
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (this.shouldShowRequestPermissionRationale(
+                    Manifest.permission.READ_CONTACTS)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+                this.requestPermissions(new String[]{Manifest.permission.READ_CONTACTS},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Log.d("AddNewContact", "Permission Granted");
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    Log.d("AddNewContact", "Permission Denied");
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    public void addNewContact(View v) {
+        // Creates a new Intent to insert a contact
+        Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
+        // Sets the MIME type to match the Contacts Provider
+        intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+
+        /* Assumes EditText fields in your UI contain an email address
+         * and a phone number.
+         *
+         */
+        selectedContactName = (EditText) findViewById(R.id.selectedContactName);
+        selectedContactNumber = (EditText) findViewById(R.id.selectedContactNumber);
+
+        /*
+         * Inserts new data into the Intent. This data is passed to the
+         * contacts app's Insert screen
+         */
+        // Inserts an email address
+        intent.putExtra(ContactsContract.Intents.Insert.NAME, selectedContactName.getText())
+        /*
+         * In this example, sets the email type to be a work email.
+         * You can set other email types as necessary.
+         */
+                .putExtra(ContactsContract.Intents.Insert.EMAIL_TYPE, ContactsContract.CommonDataKinds.Email.TYPE_WORK)
+                // Inserts a phone number
+                .putExtra(ContactsContract.Intents.Insert.PHONE, selectedContactNumber.getText())
+        /*
+         * In this example, sets the phone type to be a work phone.
+         * You can set other phone types as necessary.
+         */
+                .putExtra(ContactsContract.Intents.Insert.PHONE_TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_WORK);
+
+        /* Sends the Intent*/
+        startActivity(intent);
+    }
+
+
+    public void contactViaId(View v) {
+        getNameUsingContactId(contactID);
+    }
+
+    public void getNameUsingContactId(String contactId){
+
+        String cContactIdString = ContactsContract.Contacts._ID;
+        Uri cCONTACT_CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
+        String cDisplayNameColumn = ContactsContract.Contacts.DISPLAY_NAME;
+
+        String name = "";
+
+        String selection = cContactIdString + " = ? ";
+        String[] selectionArgs = new String[]{String.valueOf(contactId)};
+
+        Cursor cursor = getContentResolver().query(cCONTACT_CONTENT_URI, null, selection, selectionArgs, null);
+        if ((cursor != null) && (cursor.getCount() > 0)) {
+            Log.d("AddNewContact", "Cursor count: " + cursor.getCount());
+            cursor.moveToFirst();
+            while ((cursor != null) && (cursor.isAfterLast() == false)) {
+                if (cursor.getColumnIndex(cContactIdString) >= 0) {
+                    if (contactId.equals(cursor.getString(cursor.getColumnIndex(cContactIdString)))) {
+                        name = cursor.getString(cursor.getColumnIndex(cDisplayNameColumn));
+                        break;
+                    }
+                }
+                cursor.moveToNext();
+            }
+        }
+        if (cursor != null) {
+            cursor.close();
+        }
+
+        //lblNameID.setText(name);
+
+    }
+
+
+
+
+    /**
+     * Date picker that gets the date selected by the user and stores the data
+     *
+     * @param v
+     */
+    public void selectDate(View v) {
+
+        datePickerDialog = new DatePickerDialog(AddDebtActivity.this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        dateSelectedLbl.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                        mDay = dayOfMonth;
+                        mMonth = monthOfYear;
+                        mYear = year;
+                    }
+                }, mYear, mMonth, mDay);
+        datePickerDialog.show();
+    }
+
+
+    public void selectTime(View v) {
+
+        // Launch Time Picker Dialog
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay,
+                                          int minute) {
+
+                        timeSelectedLbl.setText(hourOfDay + ":" + minute);
+                    }
+                }, mHour, mMinute, false);
+        timePickerDialog.show();
+    }
+}
+
--- app/src/main/java/cs421/cs/mhu/edu/iou/IOUMain.java
+++ app/src/main/java/cs421/cs/mhu/edu/iou/IOUMain.java
@@ -1,7 +1,9 @@
 package cs421.cs.mhu.edu.iou;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class IOUMain extends Activity {
 
@@ -9,5 +11,14 @@ public class IOUMain extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ioumain);
+    }
+
+    /**
+     * Button that takes you to the ViewEditActivity
+     * @param v
+     */
+    public void addDebtBtn(View v) {
+        Intent i = new Intent(this, AddDebtActivity.class);
+        startActivity(i);
     }
 }
